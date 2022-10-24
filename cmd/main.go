acdd3fbe13dc9efcfd7033b8f9d9ed0f64c35672
OTakumi <takumi.o.0410@gmail.com>
2022-10-24 23:47:06 +0900

    Verified to work with docker

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,7 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":8000"))
 }
 
 // e.GET("/users/:id", getUser)
